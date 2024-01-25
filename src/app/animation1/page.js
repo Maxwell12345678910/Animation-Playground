b1a724416eebaeb288b0b708d1855506719003ce
Maxwell Hanson <m47hanson47@gmail.com>
2024-01-25 18:11:07 -0500

    first push to repo

---
+++ src/app/animation1/page.js
@@ -0,0 +1,43 @@
+
+
+
+
+export default function Animation(){
+
+    // const styles = {
+        
+    //     parent1:{
+    //         backgroundColor:'red'
+    //         , height:'800px'
+    //         ,width:'800px',
+    //         transition: 'background-color 0.3s', // Optional: Add transition for a smooth color change
+    //         '&hover':{
+    //             backgroundColor:'yellow'
+    //         },
+    //     },
+    //     child1:{
+    //         backgroundColor:'blue'
+    //         , height:'80px'
+    //         ,width:'80px',
+    //         transition:'transorm 1s'
+    //     },
+       
+    // };
+
+
+    return (
+        <div>
+            {/* <div style={styles.parent1}>
+                    <div style={styles.child1}>
+
+                    </div> */}
+
+                    
+                    <div className="parent1">
+                    <div className="child1">
+
+                    </div>
+            </div>
+        </div>
+    )
+}
